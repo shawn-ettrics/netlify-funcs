05015a8b4dd10fb16e0000a780bd6f95a9c765e2
shawn-ettrics <shawn@ettrics.com>
2023-03-13 23:33:05 -0700

    import paths changed

--- scripts/tab.js
+++ scripts/tab.js
@@ -1,5 +1,5 @@
-import createVerticalTabs from "https://shawn-ettrics.github.io/wildfire/scripts/createVerticalTabs.js"
-import { resizeObserver } from "https://shawn-ettrics.github.io/wildfire/scripts/helpers.js"
+import createVerticalTabs from "https://ettrics.github.io/wildfires/scripts/createVerticalTabs.js"
+import { resizeObserver } from "https://ettrics.github.io/wildfires/scripts/helpers.js"
 
 const vertTabs = document.querySelectorAll('.vertical-tabs')
 
