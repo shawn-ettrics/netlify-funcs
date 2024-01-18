7c7b7a07db9df90b03f6e98cf59c9cb6f0b9e5f3
shawn-ettrics <shawn@ettrics.com>
2024-01-18 12:41:57 -0800

    updated locations

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -15,32 +15,52 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhd25jaGkyMDciLCJhIjoiY2t2eDM4eHVoMDBmazJuc
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/shawnchi207/clohoh80p000a01r6eb5uf1ym', // Your style URL
-  center: [-116.6140, 37.0713], // Initial position [lng, lat]
-  zoom: 5 // Initial zoom
+  center: [-114.4995, 41.1391], // Initial position [lng, lat]
+  zoom: 3.6 // Initial zoom
 });
 
+function updateInfo() {
+  var zoom = map.getZoom().toFixed(2);
+  var center = map.getCenter();
+  document.getElementById('info').innerHTML = 'Zoom Level: ' + zoom + '<br>Center: ' + center.lng.toFixed(4) + ', ' + center.lat.toFixed(4);
+}
 
+// Initial info update
+updateInfo();
+
+// Add an event listener for the 'moveend' event
+map.on('moveend', updateInfo);
 
 // Coordinates and details for the markers
 var locations = [
-    {name: 'Yosemite', coordinates: [-119.5383, 37.8651], color: 'green'},
-    {name: 'Los Angeles', coordinates: [-118.2488, 34.0522], color: 'yellow'},
-    {name: 'Utah', coordinates: [-111.8910, 39.3200], color: 'orange'},
-    {name: 'Las Vegas', coordinates: [-115.1398, 36.1699], color: 'red'}
-  ];
-  
-  // Add markers to the map
-  locations.forEach(function(location) {
-    // Create a new SVG element for the marker
-    var svgMarker = document.createElement('div');
-    svgMarker.innerHTML = `<img src="https://ettrics.github.io/wildfires/assets/marker-${location.color}.svg" marker">`;
+    {name: 'Tahoe National Forest', coordinates: [-120.56273155319109, 39.56316074131553], color: 'green'},
+    {name: 'Umatilla National Forest', coordinates: [-118.66602297418554, 45.194747601460605], color: 'yellow'},
+    {name: 'Arapaho and Roosevelt National Forest', coordinates: [-105.70830509936006, 40.137517186737234], color: 'orange'},
+    {name: 'Klamath National Forest', coordinates: [-123.32754649610563, 41.501407226051214], color: 'red'},
+    {name: 'San Bernadino National Forest', coordinates: [-116.94719510161978, 34.05698272339319], color: 'green'},
+    {name: 'Tonto National Forest', coordinates: [-111.26987236420881, 33.87463158944263], color: 'yellow'},
+    {name: 'Malheur National Forest', coordinates: [-118.83517923780556, 44.12200961141153], color: 'orange'},
+    {name: 'Colville National Forest', coordinates: [-117.63219929951526, 48.63556603918802], color: 'red'}
+];
 
-    // Create the marker
-    new mapboxgl.Marker({
-      element: svgMarker.firstChild
-    })
-      .setLngLat(location.coordinates)
-      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-      .setText(location.name))
-      .addTo(map);
-  });
+  
+// Add markers to the map
+locations.forEach(function(location) {
+    if (Array.isArray(location.coordinates) && location.coordinates.length === 2) {
+        // Create a new SVG element for the marker
+        var svgMarker = document.createElement('div');
+        svgMarker.innerHTML = `<img src="https://ettrics.github.io/wildfires/assets/marker-${location.color}.svg">`;
+
+        // Create the marker
+        new mapboxgl.Marker({
+            element: svgMarker.firstChild
+        })
+        .setLngLat(location.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setText(location.name))
+        .addTo(map);
+    } else {
+        console.log(`Invalid coordinates for location: ${location.name}`);
+    }
+});
+
